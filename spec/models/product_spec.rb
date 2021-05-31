c8aa4ff6fb3b22bba850d20aabcce2ba1b536302
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-31 19:41:52 +0000

    Add product validation tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "is valid with valid attributes" do
+      category = Category.new(name: 'TestCategory',
+                              created_at: DateTime.now,
+                              updated_at: DateTime.now)
+      product = Product.new(name: 'SomeName',
+                            price: 20,
+                            quantity: 5,
+                            category: category)
+                            
+      expect(product).to be_valid
+    end
+
+    it "is unvalid without price" do
+      category = Category.new(name: 'TestCategory',
+                              created_at: DateTime.now,
+                              updated_at: DateTime.now)
+      product = Product.new(name: 'SomeName',
+                            price: nil,
+                            quantity: 5,
+                            category: category)
+
+      expect(product).to_not be_valid
+    end
+
+    it "is unvalid without name" do
+      category = Category.new(name: 'TestCategory',
+                              created_at: DateTime.now,
+                              updated_at: DateTime.now)
+      product = Product.new(name: nil,
+                            price: 20,
+                            quantity: 5,
+                            category: category)
+
+      expect(product).to_not be_valid
+    end
+
+    it "is unvalid without quantity" do
+      category = Category.new(name: 'TestCategory',
+                              created_at: DateTime.now,
+                              updated_at: DateTime.now)
+      product = Product.new(name: 'SomeName',
+                            price: nil,
+                            quantity: nil,
+                            category: category)
+
+      expect(product).to_not be_valid
+    end
+
+    it "is unvalid without category" do
+      category = Category.new(name: 'TestCategory',
+                              created_at: DateTime.now,
+                              updated_at: DateTime.now)
+      product = Product.new(name: 'SomeName',
+                            price: 20,
+                            quantity: 5,
+                            category: nil)
+      puts product.errors.full_messages.inspect
+      expect(product).to_not be_valid
+    end
+
+  end
+end
